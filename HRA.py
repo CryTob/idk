03266e9c0bb92714d329b98467de8c15f3bae903
Why do you care <88675134+CryTob@users.noreply.github.com>
2021-09-27 21:54:56 +0200

    Add files via upload

---
+++ HRA.py
@@ -0,0 +1,52 @@
+print("nacitani")
+import time
+quest = []
+time.sleep(4)
+
+print("                                                            MAD QUESTS                                                                                    ")
+print("")
+print("")
+print("Npc můžeš odpovědět ano, ne")
+
+
+
+
+#úvod
+
+print("Probudil jsi se")
+time.sleep(2)
+
+print("========================")
+print("ÚKOL: zjisti kde jsi")
+print("========================")
+
+print("Jsi v malé bílé místnosti ve které je jen jedno okno")
+time.sleep(3)
+
+print("")
+print("slyšíš kroky a v tom se oteřou dveře")
+time.sleep(3)
+
+print("")
+print("MUŽ: už jsi se probudil")
+print("skvěle")
+time.sleep(3)
+
+print("")
+print("MUŽ: chceš se najíst")
+
+najísts_se_v_úvodu = input("ano/ne")
+
+
+if najísts_se_v_úvodu == "ano":
+    print("=======================================================")
+    print("sedíš tak na schodech s chlebem a přisedne si k tobě ten muž u kterého jsi se probudil")
+    time.sleep(3)
+    print("")
+    print("kdo jste? Zeptáš se")
+    print("MUŽ: říkej mi furgim")
+    print("")
+    print("")
+    print("Jména:")
+    print("štěpán")
+    
